38e1364f905ac3f6fa45f04cceb1ea31c2656e5c
Iammilansoni <milansoni96946@gmail.com>
2025-09-04 06:42:43 +0530

    Fix deployment build issues: Remove three.js dependencies, add terser, use StarFallback, simplify vite config

---
+++ src/App.simple.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Forntpage from "./Com/Forntpage";
+
+function App() {
+  return (
+    <div className="App">
+      <Forntpage />
+      <div className="h-screen bg-black flex items-center justify-center">
+        <h1 className="text-white text-4xl">More sections coming soon...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
-// Import resource preloader for critical resources
-import './utils/resourcePreloader.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 )
